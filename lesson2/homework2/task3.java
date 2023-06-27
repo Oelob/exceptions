8d88c594507c3b7ff5d5628cc63b2a1fdb1f68ba
Ivan <ab09@inbox.ru>
2023-06-28 00:59:17 +0500

    task3

--- lesson2/homework2/task3.java
+++ lesson2/homework2/task3.java
@@ -6,7 +6,8 @@ public class task3 {
 
     /* 1. Не понятно, зачем в 8 строке нужен throws Exception, если мы говорили, что такой проброс все равно нужно обрабатывать, 
     а здесь верхняя точка в коде, ольше обрабатывать это исключение негде.
-       2. Блок с Throwable нужно перенести в самый низ, чтобы более узкие ошибки обработались ранее   */
+       2. Блок с Throwable нужно перенести в самый низ, чтобы более узкие ошибки обработались ранее 
+       3. Блок с NullPointerException лишний, в коде нет строк, где бы он мог сработать. */
         public static void main(String[] args) {
             try {
                 int a = 90;
@@ -15,8 +16,9 @@ public class task3 {
                 printSum(23, 234);
                 int[] abc = { 1, 2 };
                 abc[3] = 9;
-            } catch (NullPointerException ex) {
-                System.out.println("Указатель не может указывать на null!");
+           
+            // } catch (NullPointerException ex) {
+            //     System.out.println("Указатель не может указывать на null!");
             } catch (IndexOutOfBoundsException ex) {
                 System.out.println("Массив выходит за пределы своего размера!");
             }catch (Throwable ex) {
