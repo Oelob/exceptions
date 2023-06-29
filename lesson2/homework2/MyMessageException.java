52b061104f0f0987c870b1538055cae4e5ae5814
Ivan <ab09@inbox.ru>
2023-06-29 16:23:06 +0500

    task4

---
+++ lesson2/homework2/MyMessageException.java
@@ -0,0 +1,9 @@
+package homework2;
+
+public class MyMessageException extends Exception{
+
+    public MyMessageException(String message) {
+        super(message);
+    }
+    
+}
--- lesson2/homework2/task4.java
+++ lesson2/homework2/task4.java
@@ -1,8 +1,33 @@
 package homework2;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class task4 {
 
     /*Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку. 
     Пользователю должно показаться сообщение, что пустые строки вводить нельзя. */
     
+    public static void main(String[] args) throws MyMessageException {
+        messege();
+    }
+
+    public static String messege() throws MyMessageException{
+        String result = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите данные: ");
+        result = scanner.nextLine();
+        
+        if (result.trim() == ""){
+            
+            throw new MyMessageException("Пустую строку вводить нельзя");
+
+        }
+        
+        scanner.close();
+        return result;
+        
+    }
+
+  
 }
